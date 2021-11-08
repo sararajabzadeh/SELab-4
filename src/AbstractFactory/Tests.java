dd50f0bdd3025c98e83bbe66db751132c065198f
fforghani <fereshteh.forghani2012@gmail.com>
2021-11-09 01:58:53 +0330

    Some tests for get methods added.

--- src/AbstractFactory/Tests.java
+++ src/AbstractFactory/Tests.java
@@ -6,14 +6,28 @@ public class Tests {
     @Test
     public void testClientIran() {
         GardenCreator gardenCreator = Client.makeCreator("Iranian");
+
         assertTrue(gardenCreator.creatFlower() instanceof IranianFlower);
         assertTrue(gardenCreator.creatTree() instanceof IranianTree);
+
+        assertNotNull(gardenCreator.getTrees());
+        assertNotNull(gardenCreator.getFlowers());
+
+        assertTrue(gardenCreator.getTrees().get(0) instanceof IranianTree);
+        assertTrue(gardenCreator.getFlowers().get(0) instanceof IranianFlower);
     }
 
     @Test
     public void testClientJapan() {
         GardenCreator gardenCreator = Client.makeCreator("Japanese");
+
         assertTrue(gardenCreator.creatFlower() instanceof JapaneseFlower);
         assertTrue(gardenCreator.creatTree() instanceof JapaneseTree);
+
+        assertNotNull(gardenCreator.getTrees());
+        assertNotNull(gardenCreator.getFlowers());
+
+        assertTrue(gardenCreator.getTrees().get(0) instanceof JapaneseTree);
+        assertTrue(gardenCreator.getFlowers().get(0) instanceof JapaneseFlower);
     }
 }
